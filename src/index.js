cd50d9a283a5884acd37f3fa259b1ee7473e0d77
Oghenetega <okeremeta.tega@gmail.com>
2017-11-20 11:37:28 +0100

    serving mock data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via Api call
 getUsers().then(result => {
@@ -11,9 +11,25 @@ getUsers().then(result => {
       <td>${user.id}</td>
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
-      <td>${user.id}</td>
+      <td>${user.email}</td>
       </tr>`
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use Array.from to create a real Array from a DOM collection
+  // getElementByClassName only returns an array like object
+
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+
 });
